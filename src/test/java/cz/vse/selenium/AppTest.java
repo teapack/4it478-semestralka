ace48101dab9818e610b15d5934e479a9670344d
vjindra <vratislav_jindra@gordic.cz>
2020-01-09 20:46:52 +0100

    Initial commit

---
+++ src/test/java/cz/vse/selenium/AppTest.java
@@ -0,0 +1,22 @@
+package cz.vse.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AppTest {
+
+    private ChromeDriver chromeDriver;
+}
